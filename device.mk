a9bde935234ea58256335837369f52c359f98cf2
Mod4-Da-GOAT <omarelfarok24135@gmail.com>
2025-12-06 18:04:26 +0400

    Add dynamic partitions and update product packages

--- device.mk
+++ device.mk
@@ -6,3 +6,28 @@
 #
 
 LOCAL_PATH := device/samsung/a23
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
